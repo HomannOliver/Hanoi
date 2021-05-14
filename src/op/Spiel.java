c1d41eb21182123701547798e7f6aa050fea5510
tobiashld <tobias02173@gmail.com>
2021-05-14 12:42:52 +0200

    test commit

--- src/op/Spiel.java
+++ src/op/Spiel.java
@@ -1,8 +1,12 @@
 package op;
 
 public class Spiel {
-	
     public static void main(String[] args) {
 		System.out.println("olli ist ein kek!");
 	}
+    
+    
+    public void test() {
+    	System.out.println("olli ist immernoch ein kek!");
+    }
 }
