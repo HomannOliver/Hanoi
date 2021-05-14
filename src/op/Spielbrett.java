7ae6ccc3485c1b767355d4b888fd0fc92d7bd5ea
tobiashld <tobias02173@gmail.com>
2021-05-14 13:05:58 +0200

    Konstruktor und Attribut hinzugefügt

--- src/op/Spielbrett.java
+++ src/op/Spielbrett.java
@@ -1,5 +1,23 @@
 package op;
 
-public class Spielbrett {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Spielbrett {
+	protected List<Turm> spielbrett = new ArrayList<>();
+	
+	public Spielbrett(Turm turmLinks, Turm turmMitte, Turm turmRechts) {
+		this.spielbrett.add(turmLinks);
+		this.spielbrett.add(turmMitte);
+		this.spielbrett.add(turmRechts);
+	}
+	
+	public Spielbrett() {
+		
+	}
+	
+	protected Turm getTurm(int index) {
+		return spielbrett.get(index);
+	}
+	
 }
