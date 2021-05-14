5b26fdbb0a0988925f2e6dbfd5592c136970dcc0
Henning Escher <Henning Escher@DESKTOP-VH4BLNK>
2021-05-14 13:05:03 +0200

    stein .D: D:D :D

--- src/op/Stein.java
+++ src/op/Stein.java
@@ -2,16 +2,19 @@ package op;
 
 public class Stein {
 	
-	public Stein(int size, String farbe) { //Werte (Größe, Farbe) anfangs festlegen
+	public Stein(int size, String color) { //Werte (Größe, Farbe) anfangs festlegen
 		this.size = size;
-		this.farbe = farbe;
+		this.color = color;
 	}
 	
 	private int size; //Größe des Steins
 	
-	private String farbe; //Farbe des Steins
+	private String color; //Farbe des Steins
 	
 	public int getSize() { //Getter für Größe
 		return size;
+	}
+	public String getColor() { //Getter für Farbe
+		return color;
 	}
 }
