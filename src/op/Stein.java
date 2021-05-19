2de9b9623cb4f4b599529408990fc1858f557028
Henning Escher <Henning Escher@DESKTOP-VH4BLNK>
2021-05-19 10:55:36 +0200

    test

--- src/op/Stein.java
+++ src/op/Stein.java
@@ -2,23 +2,23 @@ package op;
 
 public class Stein {
 	
-	enum Color {
+	enum Color { //Farben, die der Stein haben kann
 		BLAU, ROT, GRÜN
 	}
 	
-	public Stein(int size, String color) { //Werte (Größe, Farbe) anfangs festlegen
+	public Stein(int size, Color c) { //Werte (Größe, Farbe) anfangs festlegen
 		this.size = size;
-		this.color = color;
+		this.color = c;
 	}
 	
 	private int size; //Größe des Steins
 	
-	private String color; //Farbe des Steins
+	private Color color; //Farbe des Steins
 	
 	public int getSize() { //Getter für Größe
 		return size;
 	}
-	public String getColor() { //Getter für Farbe
+	public Color getColor() { //Getter für Farbe
 		return color;
 	}
 }
